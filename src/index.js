61ff9a1db709128e03a3a47142963a68830988a3
saphal <bhattaraisaphal849@gmail.com>
2022-11-22 09:55:44 +0545

    about page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from "./pages/home";
+// import Home from "./pages/home";
+import About from "./pages/about";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Home />);
+ReactDOM.createRoot(document.getElementById('root')).render(<About/>);
