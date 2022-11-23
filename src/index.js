e7f29f120be9d355e1978be57efb1ce19d12a02c
Prabesh Dahal <dprabesh11@gmail.com>
2022-11-23 23:12:00 +0545

    Revert "about page (#10)" (#12)
    This reverts commit 3d2ae16f3f6feb3cb03f5fea18e5bb5a966e6a1c.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Home from "./pages/home";
-import About from "./pages/about";
+import Home from "./pages/home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.createRoot(document.getElementById('root')).render(<About/>);
+ReactDOM.createRoot(document.getElementById('root')).render(<Home />);
