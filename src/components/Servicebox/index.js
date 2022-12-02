6a5508174a98ef4743c404cd840f825f6697daec
Saphal Bhattarai <101492771+saphalbhattarai@users.noreply.github.com>
2022-12-03 00:02:41 +0545

    11/service page (#15)
    * service page

---
+++ src/components/Servicebox/index.js
@@ -0,0 +1,30 @@
+import classNames from "classnames";
+import Line from "components/Icons/line";
+import "./style.css";
+
+const Servicebox=({title,image,description,orintation})=>{
+    return(
+        <>
+        <div className='col-lg-12 padtb'>
+                <h3 className='mb-5'>{title}</h3>
+                <div className='services-box justify-content-between row'>
+                <div className= {classNames("col-lg-6 justify-content-between",
+          {'order-1':orintation==='imageLast'
+          
+        })}>
+        <div className='service-image'>
+        <img src={image} className="img-fluid mb-5"/>
+        <Line/>
+        </div>
+        </div>
+        <div className="col-lg-6">
+        <div className='service-description'>
+        <p>{description}</p>
+        </div>
+        </div>
+        </div>
+    </div>
+        </>
+    )
+}
+export default Servicebox;
