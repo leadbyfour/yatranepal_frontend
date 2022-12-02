c0818287fe30670d16135a42dc4c410b602adaab
Saphal Bhattarai <101492771+saphalbhattarai@users.noreply.github.com>
2022-12-03 00:07:41 +0545

    9/contact page (#11)
    * contact page with map component

---
+++ src/components/maps.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Maps = () => {
+  return (
+    <>
+    <iframe width="100%" height="600" 
+            id="gmap_canvas" 
+            src="https://maps.google.com/maps?q=jadibuti,kathmandu&t=&z=15&ie=UTF8&iwloc=&output=embed" 
+            frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </>
+  )
+}
+
+export default Maps;
---
+++ src/pages/contact.js
@@ -0,0 +1,94 @@
+import Navbar from 'components/Navbar/navbar';
+import Footer from 'components/Footer/footer';
+import chain from "../images/chain.png"
+import qnmark from "../images/qnmark.png"
+import plus from "../images/plus.png"
+import "../styles/contact.css";
+import Maps from 'components/maps';
+
+const Contact = () => {
+  return (
+    <>
+    <Navbar/>
+<div className='container padtb'>
+    <h2 className='red mt-3'>Contact Us</h2>
+    </div>
+    <div className='contact-banner text-white'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-lg-7 p-4'>
+                    <h5 className='red'>Call Us</h5>
+                    <p> 9851160294,9849167028,9749835510</p>
+                    <h5 className='mt-5 red'>E-Mail</h5>
+                    <p>yatranepal777@gmail.com</p>
+                    <h5 className='mt-5 red'>Location</h5>
+                    <p>Bansbari,Kathmandu</p>
+                </div>
+                <div className='col-lg-5 Touch'>
+                    <div className='form-card p-5'>
+                    <h4 className='red'>Get in Touch</h4>
+                    <p>Have some inquiry or wanna give us a feedback? Feel free to leave us a message.</p>
+                    <div className='row gy-5'>
+                        <div className='col-lg-6'>
+                        <input type="text" className="form-control form-bg" id="exampleFormControlInput1" placeholder="First Name"/>
+                        </div>
+                        <div className='col-lg-6'>
+                        <input type="text" className="form-control form-bg" id="exampleFormControlInput1" placeholder="Last Name"/>
+                        </div>
+                        <div className='col-lg-6'>
+                        <input type="text" className="form-control form-bg" id="exampleFormControlInput1" placeholder="Contact Number"/>
+                        </div>
+                        <div className='col-lg-6'>
+                        <input type="text" className="form-control form-bg" id="exampleFormControlInput1" placeholder="Address"/>
+                        </div>
+                    </div>
+                    <textarea className="form-control mt-5 form-bg text-area" id="exampleFormControlTextarea1" rows="3" placeholder='Leave Us A Message.....'></textarea>
+              		</div>
+              </div>
+          </div>
+       </div>
+   </div>
+
+<div className='container mt-5'>
+	<img src={chain} classname="img-fluid" alt="chain"/>
+	</div>
+
+<div className='banner-2'>
+    <div className="container">
+        <h1 className='mb-5 location'>Precise Location</h1>
+        <div className='row'>
+            <div className='col-lg-12'>
+            <Maps/>
+            </div>
+        </div>
+    </div>
+</div>
+			<div className='banner-3 padtb'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-6 text-center'>
+<img src={qnmark} className="img-fluid"/>
+<h4 className='text-white mt-2 frequently'>Frequently Asked Questions</h4>
+<h1 className='faq'>FAQ</h1>
+                    </div>
+                    <div className='col-lg-6'>
+                        <h5 className='red'>What is Lorem Ipsum?</h5>
+                        <p className='mt-3 faq-description'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum</p>
+                        <div className='faq-point'>
+                        <p>What is Lorem Ipsum Lorem?<img src={plus} className="img-fluid ms-2" alt="plus"/></p>
+                        <p>What is Lorem Ipsum Lorem?<img src={plus} className="img-fluid ms-2" alt="plus"/></p>
+                        <p>What is Lorem Ipsum Lorem?<img src={plus} className="img-fluid ms-2" alt="plus"/></p>
+                        <p>What is Lorem Ipsum Lorem?<img src={plus} className="img-fluid ms-2" alt="plus"/></p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+				
+<Footer/>
+    </>
+  )
+}
+
+export default Contact;
