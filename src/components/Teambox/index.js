3f8aa66a9f95d0b2dbc6a0eb504f6d0baab3a93b
saphal <bhattaraisaphal849@gmail.com>
2022-12-06 08:39:53 +0545

    change on routers

---
+++ src/components/Teambox/index.js
@@ -0,0 +1,24 @@
+import classNames from "classnames";
+import "./style.css";
+
+const Infobox=({title,description,image,color,post})=>{
+    return(
+        
+        <div className="col-lg-4">
+          <div className= {classNames("box-1 text-white",
+          {'background-grey':color==='grey',
+          'background-red':color==='red'
+        })}>
+            <div className="teambox-img text-center">
+ <img src={image} className="img-fluid" alt="image"/>
+ </div>
+ <div className="teambox-text p-5">
+ <h3 className="text-center">{title}</h3>
+ <h4 className="text-center">{post}</h4>
+ <p className='mt-4'>{description}</p>
+ </div>
+ </div>
+ </div>
+    )
+}
+export default Infobox;
