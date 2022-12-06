a8a028f14bb0bf10da622f6065adb04a33e6a8d1
Saphal Bhattarai <101492771+saphalbhattarai@users.noreply.github.com>
2022-12-06 17:54:12 +0545

    10/about page (#19)
    * change on routers
    * text intendation on about page

--- src/routers.js
+++ src/routers.js
@@ -1,6 +1,7 @@
 import Contact from './pages/contact'
 import Home from './pages/home'
 import Service from './pages/service'
+import About from './pages/about'
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
@@ -11,6 +12,7 @@ const Routers = () => {
         <Route path="/" element={<Home />} />
         <Route path="/service" element={<Service />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   )
