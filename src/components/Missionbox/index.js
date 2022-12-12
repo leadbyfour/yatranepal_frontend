83ee3a4ab24789b188a311eae6e00e534828beee
saphal <bhattaraisaphal849@gmail.com>
2022-12-12 08:23:12 +0545

    re-adding the design on about-page

--- src/components/Missionbox/index.js
+++ src/components/Missionbox/index.js
@@ -5,7 +5,7 @@ const Missionbox = ({ title, description, image, color }) => {
   return (
     <div className="col-lg-6">
       <div
-        className={classNames('box-1 text-white', {
+        className={classNames('box-mission text-white', {
           'background-grey': color === 'grey',
           'background-red': color === 'red',
         })}
@@ -14,7 +14,7 @@ const Missionbox = ({ title, description, image, color }) => {
           <img src={image} className="img-fluid" />
         </div>
         <div className="box-mission-text p-5">
-          <h3 className="text-center">{title}</h3>
+          <h3 className="text-center mt-4">{title}</h3>
           <p className="mt-4">{description}</p>
         </div>
       </div>
--- src/components/Teambox/index.js
+++ src/components/Teambox/index.js
@@ -5,7 +5,7 @@ const Teambox = ({ title, description, image, color, post }) => {
   return (
     <div className="col-lg-4">
       <div
-        className={classNames('box-1 text-white', {
+        className={classNames('box-team text-white', {
           'background-grey': color === 'grey',
           'background-red': color === 'red',
         })}
@@ -15,7 +15,7 @@ const Teambox = ({ title, description, image, color, post }) => {
         </div>
         <div className="teambox-text p-5">
           <h3 className="text-center">{title}</h3>
-          <h4 className="text-center">{post}</h4>
+          <h4 className="text-center mt-3">{post}</h4>
           <p className="mt-4">{description}</p>
         </div>
       </div>
