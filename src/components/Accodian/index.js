a43a469a3da37182c134b1a94c5e6bf9770e1efc
saphal <bhattaraisaphal849@gmail.com>
2023-02-17 07:26:36 -0800

    changes the text of accordian

--- src/components/Accodian/index.js
+++ src/components/Accodian/index.js
@@ -9,30 +9,34 @@ function BasicExample() {
           Where is Yatra Nepal Located?
         </Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        Yatra Nepal is Located on Basbari Road (In Front of British Gurkha College), Kathmandu.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
         <Accordion.Header>What are services avilable?</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        Yatra Nepal Provides:<br/>
+Periodic Service<br/>
+Minor Major Repairs<br/>
+Accidental Repairs<br/>
+Battery Service<br/>
+Pickup & Drop<br/>
+Breakdown Service<br/>
+Value Added Service<br/>
+Recondition<br/>
+Denting and Painting<br/>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="2">
         <Accordion.Header> Is Door step service avilable?</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        Yes, We offer doorstep service around Kathmandu valley.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="3">
         <Accordion.Header>Is insurance service avilable?</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        Yes, We offer different insurance polices to meet your requirements.
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
