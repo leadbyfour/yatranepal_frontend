a04f324faa4a2847d6acf93f701c34397b2543f4
user <user@usernoMacBook-Air.local>
2023-03-31 16:07:35 +0545

    10/Place all images in single folder

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -4,6 +4,14 @@ import Servicebox from 'components/Servicebox'
 import { useEffect } from 'react'
 import service from '../images/service.png'
 import '../styles/service.css'
+import AccidentalPhoto from '../images/accidental.png'
+import BatteryPhoto from '../images/battery.png'
+import BreakdownPhoto from '../images/breakdown.png'
+import LubricantPhoto from '../images/lubricant.png'
+import MinorMajorRepairPhoto from '../images/minor.png'
+import PeriodicPhoto from '../images/perodic.png'
+import PickupPhoto from '../images/pickup.png'
+import SparesPhoto from '../images/spares.png'
 
 const provideData = [
   {
@@ -53,7 +61,7 @@ const serviceData = [
   {
     id: 1,
     title: '1. Periodic Services',
-    image: '../image/perodic.png',
+    image: PeriodicPhoto,
     orintation: 'imageLast',
     description:
       'We all need to take care of our vehicles from time to time to ensure it is always up and running. Maintaining your vehicle will increase its shelf life and performance. As the wise man once said,” prevention is better than cure.”Yatra Nepal service station gives you timely reminders for servicing your two-wheeler so that you can enjoy your ride. Our timely reminders make sure that your vehicle engine is always pumping. Maintaining and taking care of your two-wheeler from time to time will also save you from a huge maintenance expense in one go. We email you, text you, and possibly anything to make sure your vehicle is fit and fine.',
@@ -61,7 +69,7 @@ const serviceData = [
   {
     id: 2,
     title: '2. Minor & Major Repairs',
-    image: '../image/minor.png',
+    image: MinorMajorRepairPhoto,
     orintation: 'imageFirst',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -69,7 +77,7 @@ const serviceData = [
   {
     id: 3,
     title: '3. Accidental Repairs',
-    image: '../image/accidental.png',
+    image: AccidentalPhoto,
     orintation: 'imageLast',
     description:
       'Accidents are unintended consequences. This accident may result in significant damage to your automobiles as well as your wallet. The number of two-wheelers in India is constantly increasing with each passing second. And, regrettably, in this environment, there is a high risk of being involved in an accident at any time. It’s preferable if we prepare ahead of time. We also provide insurance and claims for all types of two-wheelers at Yatra Nepal Service Center. Two-wheeler insurance that covers both natural (floods, lighting, self-explosion, etc.) and man-made (accident, theft, damage during a riot, etc.) damage, has a cheap premium, and add-on coverage based on your needs is excellent.',
@@ -77,7 +85,7 @@ const serviceData = [
   {
     id: 4,
     title: '4. Battery Service',
-    image: '../image/battery.png',
+    image: BatteryPhoto,
     orintation: 'imageFirst',
     description:
       'The battery is a little-regarded component in the average motorcycle – until it dies and sees the owner stranded by the roadside.Such eventualities are unpleasant – and usually avoidable.',
@@ -87,7 +95,7 @@ const serviceData = [
   {
     id: 5,
     title: '5. Breakdown Service',
-    image: '../image/breakdown.png',
+    image: BreakdownPhoto,
     orintation: 'imageLast',
     description:
       'For many people, riding is a form of therapy. It allows them to relax and enjoy the feel of the breeze in their hair. However, there is a chance that you will have a breakdown while riding your two-wheeler, which will put an end to your carefree journey. And, without assistance, one may become much more stressed.',
@@ -97,7 +105,7 @@ const serviceData = [
   {
     id: 6,
     title: '6. Pickup and Drop',
-    image: '../image/pickup.png',
+    image: PickupPhoto,
     orintation: 'imageFirst',
     description:
       'Yatra Nepal offers Roadside Assistance Services to make sure there is no stopping until you reach your destination. We provide immediate roadside service when you are stuck. These services range from mechanical breakdowns to accidental support. Be it major /minor repairs, battery change, and more we always committed to providing services to the ones in need. Our On-Road services make sure that you ride tension free under any circumstances. We are always there when you can find non',
@@ -105,7 +113,7 @@ const serviceData = [
   {
     id: 7,
     title: '7. Lubricant',
-    image: '../image/lubricant.png',
+    image: LubricantPhoto,
     orintation: 'imageLast',
     description:
       'While serving your two-wheelers, we utilize premium oils to ensure that they operate smoothly.The quality of engine oil has a significant impact on the performance, mileage, and overall shelf life of your two-wheeler. As a result, it’s critical to utilize premium lubricants on your prized two-wheeler. It is recommended that you check the oil in your two-wheeler once a month to verify that there is enough oil in it. If necessary, we recommend changing your oil to reduce friction in your vehicle’s engine. Changing your oil will improve the performance and efficiency of your vehicle, allowing it to go smoothly on any route.',
@@ -113,7 +121,7 @@ const serviceData = [
   {
     id: 8,
     title: '8. Spares Warranty',
-    image: '../image/spares.png',
+    image: SparesPhoto,
     orintation: 'imageFirst',
     description:
       'Maintenance is always thought to be costly. However, this is not the case in Yatra Nepal Service Center service centers. We make every effort to keep our premium services within reach of everyone. Whether you own a superbike or an ordinary bike, we ensure that your ride receives the best servicing in the multi-brand two-wheeler servicing market at an accessible and acceptable price. The majority of the parts fitted by Yatra Nepal Service Center are covered by a warranty. Yatra Nepal Service Center is certain that our spares will last a long time, but if one breaks, we will replace it for free within the guarantee period. ',
