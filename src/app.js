304d5d80aa4dfa748e27a9f2ea724f223defc6d2
user <user@usernoMacBook-Air.local>
2023-04-09 22:15:09 +0545

    this push request is only for testing propose

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import Navbar from 'components/Navbar/navbar'
 import Footer from 'components/Footer/footer'
+import { MessengerChat } from 'react-messenger-chat-plugin'
 const App = ({ children }) => {
   return (
     <>
       <Navbar />
       {children}
+      <div>
+        <MessengerChat
+          pageId="108384368705186"
+          bottomSpacing={300}
+          loggedInGreeting="loggedInGreeting"
+          loggedOutGreeting="loggedOutGreeting"
+          greetingDialogDisplay={'show'}
+          debugMode={true}
+          onMessengerShow={() => {
+            console.log('onMessengerShow')
+          }}
+          onMessengerHide={() => {
+            console.log('onMessengerHide')
+          }}
+          onMessengerDialogShow={() => {
+            console.log('onMessengerDialogShow')
+          }}
+          onMessengerDialogHide={() => {
+            console.log('onMessengerDialogHide')
+          }}
+          onMessengerMounted={() => {
+            console.log('onMessengerMounted')
+          }}
+          onMessengerLoad={() => {
+            console.log('onMessengerLoad')
+          }}
+        />
+      </div>
       <Footer />
     </>
   )
